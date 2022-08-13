a61e3c96f336656aed3251f5e5ed773163163c02
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-08-13 15:29:20 +0530

    Add files via upload

---
+++ input.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    name1=window.prompt("Enter youur age");
+ age=window.prompt("Enter your age");
+ document.write("Hey " +name1 +" How are You " + "You are " +age+ "Years old");
+}
+
+function myFunction2(){
+    window.alert('checked')
+}
