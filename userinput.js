fd3608ae8aa8cbc0f76f70a43d87937ed2d56379
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-08-13 17:45:31 +0530

    Add files via upload

---
+++ userinput.js
@@ -0,0 +1,9 @@
+function forgotPassword(){
+    var str=document.getElementById("text1").value;
+    var str1=document.getElementById("password").value;
+    document.write("id is : "+str+"   and password is "+str1);
+}
+function loginBtn(){
+    window.alert("login done")
+
+}
